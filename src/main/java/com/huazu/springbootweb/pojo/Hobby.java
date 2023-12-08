41fdfbff89036051af3ca7576eeaba77552602c8
zloy <421634412@qq.com>
2023-12-09 07:54:37 +0800

    最终版的项目（也许）

--- src/main/java/com/huazu/springbootweb/pojo/Hobby.java
+++ src/main/java/com/huazu/springbootweb/pojo/Hobby.java
@@ -2,6 +2,6 @@ package com.huazu.springbootweb.pojo;
 
 public class Hobby extends Object{
     static void ss(){
-        System.out.println("阿萨大大青蛙去的的");
+        System.out.println("阿萨大");
     }
 }
